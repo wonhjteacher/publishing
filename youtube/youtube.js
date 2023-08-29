4b5e175c0f9955a82707f496ac648e8dec40d36f
hyejongwon <wonhjteacher@gmail.com>
2023-08-29 16:41:27 +0900

    Add js 01

---
+++ youtube/youtube.js
@@ -0,0 +1,5 @@
+const API_KEY=`AIzaSyA6GmOmT9kTmrJd2FJ64o-gzgbodjtItpU`;
+let videoDataBox=[];
+const videoListBox = document.querySelector('.video-list');
+const searchInput = document.querySelector('#search');
+const searchBtn =  document.querySelector('#searchBtn')
