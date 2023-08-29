44eab52f4567ec6eea58727fe1c4060a00c8d70b
hyejongwon <wonhjteacher@gmail.com>
2023-08-29 16:51:44 +0900

    add js 02

--- youtube/youtube.js
+++ youtube/youtube.js
@@ -2,4 +2,14 @@ const API_KEY=`AIzaSyA6GmOmT9kTmrJd2FJ64o-gzgbodjtItpU`;
 let videoDataBox=[];
 const videoListBox = document.querySelector('.video-list');
 const searchInput = document.querySelector('#search');
-const searchBtn =  document.querySelector('#searchBtn')
+const searchBtn =  document.querySelector('#searchBtn') ; 
+
+const videoList  = async() => {
+    let url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&regionCode=kr&key=${API_KEY}`
+    let response = await fetch(url) ;
+    let videoData = await response.json();
+    videoDataBox=videoData.items;
+    console.log(videoDataBox);
+}
+
+videoList();
