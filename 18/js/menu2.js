c38b86016477a27ab9598e62e69afd509da44ac7
hyejongwon <wonhjteacher@gmail.com>
2024-02-01 13:29:31 +0900

    18-6

--- 18/js/menu2.js
+++ 18/js/menu2.js
@@ -20,5 +20,21 @@ star.forEach(ele=>{
 })
 
 // 아코디언 메뉴 
+contentList.addEventListener('click',e=>{
+  let selectedLi=e.target;
+  let isOn = selectedLi.classList.contains('on');
+  if(isOn){ // 현재 열린상태 => 닫힘으로 만든다
+    selectedLi.classList.remove('on');
+    selectedLi.childNodes.forEach(ele=>{
+      if(ele.tagName === "P") ele.classList.remove('active')
+    })
+  }else{ // 현재 닫힘 상태 => 열림으로 만든다
+    content.forEach(ele=>ele.classList.remove('on'))
+    selectedLi.classList.add('on');
+    contentBody.forEach(ele=>ele.classList.remove('active'));
+    let index = selectedLi.dataset.index;
+    contentBody[index].classList.add('active');
+  }
+})
 
  
