80536361a11812b974fe546df93167a87488eb5c
hyejongwon <wonhjteacher@gmail.com>
2024-02-06 10:08:16 +0900

    21-2

---
+++ 21/js/youtube.js
@@ -0,0 +1,19 @@
+const API_KEY = `AIzaSyBuBKfs4MHbSdcrnL0F249dZOs5gvSuVH`;
+let videoDataBox=[];
+const videoListBox =  document.querySelector('.video-list');
+const searchInput = document.querySelector('#search');
+const searchBtn = document.querySelector('#searchBtn')
+//검색창에서 검색 시 검색 결과 불러오는 함수 
+const getSearchVideo  = () => {
+
+}
+
+//비디오 리스트를 불러오는 함수 
+const videoList = () => {
+
+}
+
+//화면에 그리는 render 함수 
+const render  = () =>{
+
+}
