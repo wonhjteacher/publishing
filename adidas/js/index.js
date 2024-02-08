a25ce027024c3e73b333d5e9937b194678a72ee4
hyejongwon <wonhjteacher@gmail.com>
2024-02-08 14:30:09 +0900

    22-5

--- adidas/js/index.js
+++ adidas/js/index.js
@@ -1,6 +1,14 @@
 
 var swiper = new Swiper(".product-tab-Swiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+  slidesPerView: "auto",
+  spaceBetween: 8,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
 });
