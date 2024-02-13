f346cf7376aaf75390e791fea73190d923e7238e
hyejongwon <wonhjteacher@gmail.com>
2024-02-13 14:02:56 +0900

    23-6

--- adidas/js/index.js
+++ adidas/js/index.js
@@ -38,5 +38,32 @@ noticeClose.addEventListener('click',()=>{
 })
 
 /* gnb 처리 */
-const depth1Li =  document.querySelectorAll('.gnb>li');
-const depth2Bg = document.querySelectorAll('.dep2-bg-box');
+const gnb = document.querySelector(".gnb");
+const depth1 = gnb.querySelectorAll("li"); //  gnb 하위의 li 요소를 모두 찾음
+//const depth1 =  document.querySelectorAll('.gnb>li');
+
+depth1.forEach(function(ele){
+  // 마우스가 li 요소 위에 올려졌을때 
+  ele.addEventListener("mouseenter",function(){
+    //현재li 요소의 자식 중에 클래스가 dep2-bg-box인요소를 찾아서 보이게 한다. 
+    let depth2 = this.querySelector(".dep2-bg-box");
+    if(depth2){
+      depth2.style.display = "block";
+    }
+  })
+  // 마우스가 li 요소를 벗어났을 떄 
+  ele.addEventListener("mouseleave",function(){
+    //현재li 요소의 자식 중에 클래스가 dep2-bg-box인요소를 찾아서 숨김
+    let depth2 = this.querySelector(".dep2-bg-box");
+    if(depth2){
+      depth2.style.display = "none";
+    }
+  })
+
+})
+
+
+
+
+
+
