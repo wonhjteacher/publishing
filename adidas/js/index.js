ba00c2701ce081a0fac3c762db173aae32a4f305
hyejongwon <wonhjteacher@gmail.com>
2024-02-13 12:21:17 +0900

    23-5

--- adidas/js/index.js
+++ adidas/js/index.js
@@ -23,3 +23,20 @@ var swiper2 = new Swiper(".product-popular-Swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+/* 공지모달 처리 */ 
+
+const noticeOpen = document.querySelector('.btn-open-notice');
+const noticeClose = document.querySelector('.close-btn-box');
+const noticeWindow= document.querySelector('#noticeModalBg');
+
+noticeOpen.addEventListener('click',()=>{
+  noticeWindow.style.display='block';
+})
+noticeClose.addEventListener('click',()=>{
+  noticeWindow.style.display='none';
+})
+
+/* gnb 처리 */
+const depth1Li =  document.querySelectorAll('.gnb>li');
+const depth2Bg = document.querySelectorAll('.dep2-bg-box');
