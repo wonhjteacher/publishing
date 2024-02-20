f97be629ca607477e5cf739fe5f8edbbe6b1af25
hyejongwon <wonhjteacher@gmail.com>
2024-02-20 12:07:40 +0900

    28-3

--- jQuery-4day/museum/js/museum.js
+++ jQuery-4day/museum/js/museum.js
@@ -33,9 +33,44 @@ $('.slider1 img').height(winHeight+(winHeight*0.3));
   /*fullpage*/
   $('#fullpage').fullpage({
     licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    menu: '.right_menu',
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
     afterRender: function(){
       $('.slogon_title').delay(200).animate({top:'50px',opacity:1},800)
       $('.slogon_txt').delay(800).animate({top:'50px',opacity:1},800)
-    }
-  });
+    },
+    afterLoad: function(origin, destination, direction, trigger){
+      if(destination.index==0 || destination.index==2){
+        $('.header').removeClass('up');
+        $('#right_nav').removeClass('up');
+        //$('.section2 .inner,.section4 .inner').css({paddingTop:'300px',opacity:0});
+      }else if(destination.index==1|| destination.index==3){
+        $('.header').addClass('up'); 
+        $('#right_nav').addClass('up'); 
+      }
+
+      // 1,3 번 섹션의 내용 보이게 하기 
+      if(destination.index==1){
+        $('.section2 .inner').animate({paddingTop:'100px',opacity:1})
+      }else if(destination.index==3){
+        $('.section4 .inner').animate({paddingTop:'100px',opacity:1})
+      }
+    }//end :  afterLoad 콜백
+
+  }); //end: fullpage 
+
+   /*섹션2 슬라이더 slick 슬라이더*/
+   $('.slider2').slick({
+      dots:true,
+      autoplay:true,
+      speed:300,
+      autoplaySpeed: 2000,
+      arrows: false,
+   })
+   $('.play').on('click',function(){
+      $('.slider2').slick('slickPlay')
+   })
+   $('.pause').on('click',function(){
+     $('.slider2').slick('slickPause')
+   })
 })//jQ
